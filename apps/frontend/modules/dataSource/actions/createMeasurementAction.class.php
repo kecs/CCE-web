35734d0b34e3af6d70123ee3e2ca3fb97c8d33cd
Cassus Adam Banko <banko.adam@gmail.com>
2010-12-21 03:13:42 +0100

    rename datasource module to dataSource

--- apps/frontend/modules/dataSource/actions/createMeasurementAction.class.php
+++ apps/frontend/modules/dataSource/actions/createMeasurementAction.class.php
@@ -8,13 +8,13 @@ class DataSource_createMeasurementAction extends sfAction
    */
   public function execute($request)
   {
-    $this->forward404Unless($dataSource = $this->getRoute()->getObject());
+    $this->forward404Unless($dataSource = $this->getRoute()->getObject()); /* @var $dataSource DataSource */
     $channel = $request->getParameter('channel');
 
     $typeReflectionClass = new ReflectionClass($channel);
     if ($typeReflectionClass->isSubclassOf('Measurement'))
     {
-      $measurement = new $channel($dataSource, $request);
+      $measurement = $channel::create($dataSource, $request);
       $measurement->save();
       return $this->renderText('OK');
     }
