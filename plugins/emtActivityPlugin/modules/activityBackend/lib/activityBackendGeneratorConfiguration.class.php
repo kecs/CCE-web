f649c8066508b0ad943d924cebf49f6ce377f131
Cassus Adam Banko <banko.adam@gmail.com>
2010-12-22 16:06:31 +0100

    Activity backend megvan, külön a saját plugin-ben

---
+++ plugins/emtActivityPlugin/modules/activityBackend/lib/activityBackendGeneratorConfiguration.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * activityBackend module configuration.
+ *
+ * @package    cce
+ * @subpackage activityBackend
+ * @author     Adam Banko (Cassus)
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class activityBackendGeneratorConfiguration extends BaseActivityBackendGeneratorConfiguration
+{
+}
---
+++ plugins/emtActivityPlugin/modules/activityBackend/lib/activityBackendGeneratorHelper.class.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * activityBackend module helper.
+ *
+ * @package    cce
+ * @subpackage activityBackend
+ * @author     Adam Banko (Cassus)
+ * @version    SVN: $Id: helper.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class activityBackendGeneratorHelper extends BaseActivityBackendGeneratorHelper
+{
+}
