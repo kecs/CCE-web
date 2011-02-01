69a1c706c502f40af87a2dd2f06872ca52c60bbd
Cassus Adam Banko <banko.adam@gmail.com>
2011-02-01 21:17:29 +0100

    Egérgörgős zoom működik, drag to scroll is. Closes #13

--- apps/frontend/modules/entity/templates/indexRecursiveSuccess.js
+++ apps/frontend/modules/entity/templates/indexRecursiveSuccess.js
@@ -29,33 +29,74 @@
       this.entityChannel.data('channelObj', this);
       this.chart = new Highcharts.Chart(this.getInitOptions());
 
-      this.entityChannel.bind('mousewheel', function(event, delta, deltaX, deltaY) {
-        if (deltaY === 0) {
-          return;
-        }
-        event.preventDefault();
-        var currentTimePeriod = channel.getTimePeriod();
-        var dt = currentTimePeriod.to - currentTimePeriod.from;
-
-        var newTimePeriod;
-        if (deltaY > 0) {
-          newTimePeriod = {
-            from: currentTimePeriod.from + dt/3,
-            to: currentTimePeriod.to - dt/3
-          };
-        }
-        if (deltaY < 0) {
-          newTimePeriod = {
-            from: currentTimePeriod.from - dt,
-            to: currentTimePeriod.to + dt
-          };
-        }
-        channel.zoomTo(newTimePeriod);
+      this.entityChannel.bind('mousewheel', channel.mouseWheelEvent);
+      this.entityChannel.draggable({
+        axis: 'x',
+        distance: 30,
+        helper: function () {
+          return $('<div />');
+        },
+        start: channel.dragStartEvent,
+        drag: channel.dragEvent,
+        stop: channel.dragStopEvent
       });
     };
 
     channel.entityChannel = undefined;
     channel.chart = undefined;
+
+    channel.pagePositionToAxisValues = function (position) {
+      return {
+        x: channel.chart.xAxis[0].translate(position.left - channel.entityChannel.offset().left, true),
+        y: channel.chart.yAxis[0].translate(position.top - channel.entityChannel.offset().top, true)
+      };
+    };
+
+    (function () {
+      var mouseValues;
+      channel.dragStartEvent = function (event,ui) {
+        mouseValues = channel.pagePositionToAxisValues(ui.originalPosition);
+      };
+      channel.dragStopEvent = function (event,ui) {
+        var extremes = channel.chart.xAxis[0].getExtremes();
+        channel.zoomTo({
+          from: extremes.min,
+          to: extremes.max
+        });
+      };
+      channel.dragEvent = function (event,ui) {
+        var currentMouseValues = channel.pagePositionToAxisValues(ui.position);
+        var dx = mouseValues.x - currentMouseValues.x;
+
+        var extremes = channel.chart.xAxis[0].getExtremes();
+        channel.chart.xAxis[0].setExtremes(extremes.min + dx, extremes.max + dx, true, false);
+      //not updating mouseValues as we translated to chart so that the current mouse position matches it
+      };
+    }());
+
+    channel.mouseWheelEvent = function(event, delta, deltaX, deltaY) {
+      if (deltaY === 0) {
+        return;
+      }
+      event.preventDefault();
+      var currentTimePeriod = channel.getTimePeriod();
+      var dt = currentTimePeriod.to - currentTimePeriod.from;
+
+      var newTimePeriod;
+      if (deltaY > 0) {
+        newTimePeriod = {
+          from: currentTimePeriod.from + dt/3,
+          to: currentTimePeriod.to - dt/3
+        };
+      }
+      if (deltaY < 0) {
+        newTimePeriod = {
+          from: currentTimePeriod.from - dt,
+          to: currentTimePeriod.to + dt
+        };
+      }
+      channel.zoomTo(newTimePeriod);
+    };
 
     channel.getTimePeriod = function () {
       var extremes = channel.chart.xAxis[0].getExtremes();
@@ -64,17 +105,16 @@
         to: extremes.max
       };
     };
-    
     channel.update = function (timePeriod) {
-      channel.chart.xAxis[0].setExtremes(timePeriod.from, timePeriod.to);
       channel.chart.showLoading();
       jQuery.get($.urlTemplate('<?php echo url_for("measurement_data", array("type" => ":type", "id" => ":id", "channel" => ":channel")) ?>').generate({
         id: this.getEntityId(),
         type: this.getEntityType(),
         channel: this.getChannelType()
       }), timePeriod, function (data) {
-        channel.doUpdate(data);
         channel.chart.hideLoading();
+        channel.chart.xAxis[0].setExtremes(timePeriod.from, timePeriod.to);
+        channel.doUpdate(data);
       });
     };
 
@@ -107,7 +147,7 @@
       return {
         chart: {
           renderTo: this.entityChannel[0],
-          zoomType: 'x',
+          zoomType: '',
           marginTop: 10,
           marginRight: 10,
           marginBottom: 30,
@@ -126,6 +166,9 @@
         plotOptions: {
           series: {
         }
+        },
+        loading: {
+          showDuration: 0
         },
         title: {
           text: ""
