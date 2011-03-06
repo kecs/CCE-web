d681670ae2ebf5e99e46c346482dbfbfb8f1eee5
Cassus Adam Banko <banko.adam@gmail.com>
2011-03-06 18:13:21 +0100

    Mérési adatok listázásakor lászik a fa struktúra behúzásokkal. Ha nincs adat, akkor a leveleknél ezt ki is írjuk.
    Closes #33

--- apps/frontend/modules/entity/templates/indexRecursiveSuccess.php
+++ apps/frontend/modules/entity/templates/indexRecursiveSuccess.php
@@ -4,12 +4,18 @@
 <h1><?php echo __('Measurements') ?></h1>
 <?php foreach ($descendantEntities as $entity): /* @var $entity Entity */ ?>
   <div class="entity" data-type="<?php echo $entity->type ?>" data-id="<?php echo $entity->id ?>">
-    <h2><?php echo $entity ?></h2>
+    <h2 style="margin-left: <?php echo $entity->getNode()->getLevel() * 15 ?>px"><?php echo $entity ?></h2>
     <div class="details">
+    <?php if (count($activeChannels = $entity->getActiveChannels())): ?>
     <?php foreach ($entity->getActiveChannels() as $channel): ?>
-      <div class="channel chart" data-channel="<?php echo $channel ?>">
-      </div>
+        <div class="channel chart" data-channel="<?php echo $channel ?>">
+        </div>
     <?php endforeach ?>
-    </div>
-  </div>
+    <?php elseif (!$entity->getNode()->hasChildren()): ?>
+          <div class="note">
+      <?php echo __('No data') ?>
+        </div>
+    <?php endif ?>
+        </div>
+      </div>
 <?php endforeach ?>
