d9d966a5d94bd8320cd277f97394a827563debf6
Cassus Adam Banko <banko.adam@gmail.com>
2011-03-06 17:16:31 +0100

    Entity fa admin felületen az entity törléskör van felugró kérdés.
    Closes #38

--- apps/backend/modules/entity/templates/_nested_set_list.php
+++ apps/backend/modules/entity/templates/_nested_set_list.php
@@ -65,7 +65,7 @@
                     var position = data.rslt.p;
                     var rollback = data.rlbk;
                     $('.error, .notice').remove();
-                    $('.nested_set_manager_holder').before('<div class="waiting"><?php echo __('Sending data to server.'); ?></div>');
+                    $('.nested_set_manager_holder').before('<div class="waiting"><?php echo __('Sending data to server.') ?></div>');
                     $.ajax({
                       type: "POST",
                       url : $.urlTemplate('<?php echo url_for('entity_move', array('id' => ':id')) ?>').generate({id: getId(movedNode)}),
@@ -78,23 +78,28 @@
                         $('.waiting').remove();
                       },
                       success : function (data, textStatus) {
-                        $('.nested_set_manager_holder').before('<div class="notice"><?php echo __('The item was moved successfully.'); ?></div>');
+                        $('.nested_set_manager_holder').before('<div class="notice"><?php echo __('The item was moved successfully.') ?></div>');
                         jQuery.jstree._reference(referenceNode).open_node(referenceNode);
                       },
                       error : function (data, textStatus) {
-                        $('.nested_set_manager_holder').before('<div class="error"><?php echo __('Error while moving the item.'); ?></div>');
-          $.jstree.rollback(rollback);
-        }
-      });
-    };
+                        $('.nested_set_manager_holder').before('<div class="error"><?php echo __('Error while moving the item.') ?></div>');
+                        $.jstree.rollback(rollback);
+                      }
+                    });
+                  };
 
-    $("#entity-nested-set").jstree({
-      plugins : [ "themes", "html_data", 'dnd' ]
-    })
-    .jstree('open_all')
-    .bind('move_node.jstree', onMove);
+                  $("#entity-nested-set").jstree({
+                    plugins : [ "themes", "html_data", 'dnd' ]
+                  })
+                  .jstree('open_all')
+                  .bind('move_node.jstree', onMove);
 
-  });
+                  $(".nodeinteraction.deletenode > form").submit(function (eventData) {
+                    return confirm('<?php echo __('Are you sure?') ?>');
+                  });
+
+                });
+<?php /* NetBeans indention fix */ ?>
   /* ]]> */
 </script>
 
