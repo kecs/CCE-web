f5528d32bbd8be53ca237ae25f0683d912ccbb28
Cassus Adam Banko <banko.adam@gmail.com>
2011-07-06 19:08:42 +0200

    mérés oldal fa helyett /-el elválaszós felsorolásként jeleníti meg az adatforrásokat
    Closes #54

--- apps/frontend/modules/entity/templates/indexRecursiveSuccess.php
+++ apps/frontend/modules/entity/templates/indexRecursiveSuccess.php
@@ -3,19 +3,15 @@
 
 <h1><?php echo __('Measurements') ?></h1>
 <?php foreach ($descendantEntities as $entity): /* @var $entity Entity */ ?>
-  <div class="entity" data-type="<?php echo $entity->type ?>" data-id="<?php echo $entity->id ?>">
-    <h2 style="margin-left: <?php echo $entity->getNode()->getLevel() * 15 ?>px"><?php echo $entity ?></h2>
-    <div class="details">
-    <?php if (count($activeChannels = $entity->getActiveChannels())): ?>
-    <?php foreach ($entity->getActiveChannels() as $channel): ?>
-        <div class="channel chart" data-channel="<?php echo $channel ?>">
-        </div>
-    <?php endforeach ?>
-    <?php elseif (!$entity->getNode()->hasChildren()): ?>
-          <div class="note">
-      <?php echo __('No data') ?>
-        </div>
-    <?php endif ?>
-        </div>
+  <?php if (count($activeChannels = $entity->getActiveChannels())): ?>
+    <div class="entity" data-type="<?php echo $entity->type ?>" data-id="<?php echo $entity->id ?>">
+      <h2><?php echo $entity->getNode()->getPath(' / ') ?></h2>
+      <div class="details">
+        <?php foreach ($entity->getActiveChannels() as $channel): ?>
+          <div class="channel chart" data-channel="<?php echo $channel ?>">
+          </div>
+        <?php endforeach ?>
       </div>
+    </div>
+  <?php endif ?>
 <?php endforeach ?>
