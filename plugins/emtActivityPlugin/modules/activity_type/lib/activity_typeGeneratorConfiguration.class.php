04e1d2ff1d85a183f79564ca3a7e1420134788b1
Mihály Gábor <kukikapitany@gmail.com>
2012-02-03 11:15:22 +0100

    config liles for activity type backend

---
+++ plugins/emtActivityPlugin/modules/activity_type/lib/activity_typeGeneratorConfiguration.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * activationBackend module configuration.
+ *
+ * @package    cce
+ * @subpackage activationBackend
+ * @version    SVN: $Id: configuration.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class activity_typeGeneratorConfiguration extends BaseActivity_typeGeneratorConfiguration
+{
+}
